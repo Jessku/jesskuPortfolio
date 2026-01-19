1759912e5a64ed2afcbec1d11012acea70cfceab
Jessee Horton <61556428+Jessku@users.noreply.github.com>
2026-01-19 16:11:29 -0500

    Added picture of me and tab icon

--- components/SocialBar/SocialBar.tsx
+++ components/SocialBar/SocialBar.tsx
@@ -3,11 +3,15 @@ import '../../style/stylesheet.css';;
 import { ActionIcon, ActionIconGroup } from "@mantine/core"
 import { IconBrandGithub, IconBrandLinkedin, IconBrandTwitch, IconMail, IconPaperclip } from '@tabler/icons-react'
 import { socials } from "../../models/models";
+import Image from 'next/image';
 
 export default function SocialBar() {
     return (
         <div>
             <text> Jessee Horton</text>
+            <div>
+                <Image src={"/Jessee.jpg"} alt="Jessee Horton" width={45} height={45}/>
+            </div>
 
             <ActionIconGroup>
                 <ActionIcon variant="default" size="lg" aria-label="name" color="grape" onClick={() => window.open(socials.resume)}>
