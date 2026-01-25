9f3b8f93d671dc542f5d96c8efd19009a73f588c
Jessee Horton <61556428+Jessku@users.noreply.github.com>
2026-01-24 20:19:52 -0500

    added KH font

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import {
 import { theme } from "../theme";
 
 export const metadata = {
-  title: "Mantine Next.js template",
-  description: "I am using Mantine with Next.js!",
+  title: "Jessee Horton",
+  description: "Jessku Hatsune",
 };
 
 export default function RootLayout({ children }: { children: any }) {
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { ActionIcon, ActionIconGroup } from "@mantine/core"
 import {IconMail, IconPaperclip} from '@tabler/icons-react'
 import { socials } from "../models/models";
 import SocialBar from './../components/SocialBar/SocialBar';
-import StickyCards from './../StickyCards/StickyCards';
+//import  StickyCards  from './../StickyCards/StickyCards';
 
 export default function HomePage() {
   return (
@@ -13,7 +13,7 @@ export default function HomePage() {
   <div> 
     <SocialBar/>
     <div>
-      <StickyCards/>
+      
     </div>
 
   </div>
