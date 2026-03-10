914d1a1d9268afc74b32791edc757bd788a473fe
Jessee Horton <61556428+Jessku@users.noreply.github.com>
2026-03-10 09:50:41 -0400

    updated resume link

--- models/models.tsx
+++ models/models.tsx
@@ -2,7 +2,7 @@ export const socials = {
     title: "Jessku Prototype",
     linkedin: "https://www.linkedin.com/in/jessee-horton-719088173/",
     email: "jesshorton2000@gmail.com",
-    resume: "https://docs.google.com/document/d/1PJ4-Y3XtmUvdguW2_SDotKAVWCQtC0WNbdHhbgKE3gs/edit?usp=sharing",
+    resume: "https://drive.google.com/file/d/15_R7ViZwP16FilhnV8JYWozM_VPk-wjt/view?usp=sharing",
     twitch: "https://www.twitch.tv/jessku_h",
     github: "https://github.com/jessku",
 };
